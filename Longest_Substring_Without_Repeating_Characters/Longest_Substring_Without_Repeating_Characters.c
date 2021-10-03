2e1f0a8bb4f4e96b44d7b41ddbd99016ed7c8260
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-10-03 23:39:48 +0800

    Update Longest_Substring_Without_Repeating_Characters.c

--- Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.c
+++ Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.c
@@ -1,36 +1,45 @@
-int lengthOfLongestSubstring(char * s){
+#include <stdlib.h>
+#include <stdio.h>
+
+//3. Longest Substring Without Repeating Characters
+int lengthOfLongestSubstring(char* s) {
     int max = 0;
     int temp_max = 0;
     int current = 0;
     int repeating_char = 0;
     int new_start = 0;
     int flag = 0;
-    
-    if(s[current] == '\0') return 0;
-
-    while (s[current] != '\0'){
-        
-       	for (repeating_char = current-1; repeating_char >= new_start; repeating_char--){
-			if (s[repeating_char] == s[current]){
-				flag = 1;
-				break;
-			}
-		} 
-        
-        
-        if (flag == 0) {
-			temp_max++;
+    if (s[current] == '\0') return 0;
+    while (s[current] != '\0') {
+        for (repeating_char = current - 1; repeating_char >= new_start; repeating_char--) {
+            if (s[repeating_char] == s[current]) {
+                flag = 1;
+                break;
+            }
         }
-		else {
-			new_start = repeating_char + 1;
-			temp_max = current - new_start + 1;
-			flag = 0;
-		}        
-        
+        if (flag == 0) {
+            temp_max++;
+        }
+        else {
+            new_start = repeating_char + 1;
+            temp_max = current - new_start + 1;
+            flag = 0;
+        }
         if (temp_max > max)	max = temp_max;
-		current++;
-        
+        current++;
     }
-    
     return max;
+}
+
+
+void main() {
+
+    //3. Longest Substring Without Repeating Characters
+    
+    char* s = (char*)malloc(sizeof(char) * 10);
+    s = "abcabcbb";
+    //printf("%s\n", s);
+    printf("%d\n", lengthOfLongestSubstring(s));
+    
+
 }
